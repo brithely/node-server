10249bcd7fbb8decf0b7e96131dc8a5c4bf99403
jungboro <bing9013@naver.com>
2020-01-30 23:39:42 +0900

    프로젝트 틀 완성

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parse');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const pageRouter = require('./routes/page')
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8001);
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParse(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  }
+}));
+app.use(flash());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/test/:id', (req, res) => {
-  console.log(req.params, req.query);
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404;
+  next(err);
 });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+})
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), ' port number listening');
 });
 
-module.exports = app;
---
+++ routes/page.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: 'My Info' });
+});
+
+router.get('/join', (req, res) => {
+    res.render()
+});
