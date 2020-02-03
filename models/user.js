e28c602ff7d7df8f80b429c6cc5a23b3aa14bd0e
jungboro <bing9013@naver.com>
2020-02-03 23:42:33 +0900

    sequelize 제거 및 유저 모델 및 유저 가입 추가

--- models/user.js
+++ models/user.js
@@ -1,29 +1,111 @@
-module.exports = (sequelize, DataTypes) => (
-    sequelize.define('user', {
-        email: {
-            type: DataTypes.STRING(40),
-            allowNull: false,
-            unique: true,
-        },
-        name: {
-            type: DataTypes.STRING(20),
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING(100),
-            allowNull: true,
-        },
-        provider: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-            defaultValue: 'local',
-        },
-        snsId: {
-            type: DataTypes.STRING(30),
-            allowNull: true,
-        },
-    }, {
-        timpstamps: true,
-        paranoid: true,
-    })
-);
+const sql = require("./db.js");
+
+// constructor
+const User = function (user) {
+    this.email = user.email;
+    this.name = user.name;
+    this.password = user.password;
+};
+
+User.create = (newUser, result) => {
+    console.log(newUser);
+    sql.query("INSERT INTO User SET ?", newUser, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        console.log("created user: ", { id: res.insertId, ...newUser });
+        result(null, { id: res.insertId, ...newUser });
+    });
+};
+
+User.findById = (userId, result) => {
+    sql.query(`SELECT * FROM User WHERE id = ${userId}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+User.getAll = result => {
+    sql.query("SELECT * FROM User", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        console.log("users: ", res);
+        result(null, res);
+    });
+};
+
+User.updateById = (id, user, result) => {
+    sql.query(
+        "UPDATE User SET email = ?, name = ?, active = ? WHERE id = ?",
+        [user.email, user.name, user.active, id],
+        (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+                return;
+            }
+
+            if (res.affectedRows == 0) {
+                // not found User with the id
+                result({ kind: "not_found" }, null);
+                return;
+            }
+
+            console.log("updated user: ", { id: id, ...user });
+            result(null, { id: id, ...user });
+        }
+    );
+};
+
+User.remove = (id, result) => {
+    sql.query("DELETE FROM User WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        if (res.affectedRows == 0) {
+            // not found User with the id
+            result({ kind: "not_found" }, null);
+            return;
+        }
+
+        console.log("deleted user with id: ", id);
+        result(null, res);
+    });
+};
+
+User.removeAll = result => {
+    sql.query("DELETE FROM User", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        console.log(`deleted ${res.affectedRows} users`);
+        result(null, res);
+    });
+};
+
+module.exports = User;
