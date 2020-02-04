eda373bc090f6c7460c600de01a338b7bdb71e5a
jungboro <bing9013@naver.com>
2020-02-05 00:00:42 +0900

    불필요한 내용 삭제

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ require('./models/db.js');
 
 const userRouter = require('./routes/users');
 
-app.get("/", (req, res) => {
-  res.json({ message: "Simple Application." });
-});
-
 app.use("/api/", userRouter);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
 });
