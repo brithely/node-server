dc9bff43a0672c73ffd2952084747d96ed998dcf
jungboro <bing9013@naver.com>
2020-02-13 23:33:02 +0900

    프로젝트 구조 변경-1

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 
+const passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'passwd',
+  session: false,
+  function(username, password, done) {
+  }
+}));
+
+
+
 const app = express();
 
 app.use(express.json());
