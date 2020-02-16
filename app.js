e3c1af2fec0712316f93611ef4d80b8be07f21e9
jungboro <bing9013@naver.com>
2020-02-16 23:13:46 +0900

    Todo get, getAll, insert(일부) 완료

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 
-const passport = require('passport')
-, LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'passwd',
-  session: false,
-  function(username, password, done) {
-  }
-}));
-
-
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-require('./models/db.js');
+// require('./models/db.js');
 
-const userRouter = require('./routes/users');
+// const userRouter = require('./routes/users');
+const todoRouter = require('./src/routes/todo-route')
 
-app.use("/api/", userRouter);
+// app.use("/api/", userRouter);
+app.use("/api/todos", todoRouter);
 
 app.listen(3000, () => {
 });
