e4295afc40fb89b297b21404d63cd6b1d0cb4911
정승룡 <37111838+brithely@users.noreply.github.com>
2020-02-19 20:36:56 +0900

    Backend (#1)
    * 프로젝트 구조 변경-1
    
    * Todo get, getAll, insert(일부) 완료
    
    * 불필요한 파일 삭제

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-require('./models/db.js');
+// require('./models/db.js');
 
-const userRouter = require('./routes/users');
+// const userRouter = require('./routes/users');
+const todoRouter = require('./src/routes/todo-route')
 
-app.use("/api/", userRouter);
+// app.use("/api/", userRouter);
+app.use("/api/todos", todoRouter);
 
 app.listen(3000, () => {
 });
