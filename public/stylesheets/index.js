53e1148b9e6cb0d688920fe54b50e108ca4e36b8
kytsaaa6 <kytsaaa6@naver.com>
2020-03-01 18:38:26 +0900

    Todo JS - JS ES5 -> Jquery + ES6 변경

--- public/stylesheets/index.js
+++ public/stylesheets/index.js
@@ -1,131 +1,167 @@
 // ToDoList JS
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 // 문서 로드 시 실행 이벤트 정의
-window.onload = function() {
-    
+$(document).ready(function() {
+
     // 저장 버튼 이벤트
-    document.getElementById('saveBtn').addEventListener('click', TodoListController.createTodoAjax());
+    $("#addBtn").click(function() {
+        TodoListController.createTodoAjax();
+    });
+
     // 수정 버튼 이벤트
-    document.getElementById('editBtn').addEventListener('click', TodoListController.updateTodoView());
+    $("#editBtn").click(function() {
+        TodoListController.updateTodoView();
+    });
+
+    // 저장 버튼 이벤트
+    $("#saveBtn").click(function() {
+        TodoListController.updateTodoAjax;
+    });
+
+    // 취소 버튼 이벤트
+    $("#cancelBtn").click(function() {
+        TodoListController.cancelUpdate;
+    });
+
     // 삭제 버튼 이벤트
-    document.getElementById('removeBtn').addEventListener('click', TodoListController.deleteTodoAjax());
+    $("#removeBtn").click(function() {
+        TodoListController.deleteTodoAjax();
+    });
+
     // 모두 삭제 버튼 이벤트
-    document.getElementById('allDelBtn').addEventListener('click', TodoListController.initTodo());
-
-//    document.getElementById('').addEventListener('click', TodoListController.initTodo());
-
-//   document.getElementById('').addEventListener('click', TodoListController.initTodo());
-
-//    document.getElementById('').addEventListener('click', TodoListController.initTodo());
-
-//    document.getElementById('').addEventListener('click', TodoListController.initTodo());
-
+    $("#allDelBtn").click(function() {
+        TodoListController.allDelTodoAjax();
+    });
 
     TodoListController.initTodo();
-}
 
+});
+    
 var TodoListController = {
 
-	initTodo : function() {
+    initTodo : function() {
 
         TodoListController.readTodoAjax();
 
-	}, readTodoAjax : function() {
+    }, readTodoAjax : function() {
+              
+        const param = {
+            userId: $('#userId').val()
+        }
+
+        $.ajax({
+
+            url: '${pageContext.request.contextPath}',
+            type: 'GET',
+            data: param,
+            //dataType: ,
+            success: function(data) {
                 
-        //객체 생성부분
-        var xmlhttp; 
-        if (window.XMLHttpRequest) {  
-            xmlhttp = new XMLHttpRequest();
-        } 
-        else {  
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
+                let listHtml = '';
 
-        //Ajax구현부분
-        xmlhttp.onreadystatechange = function() {  
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                //통신 성공시 구현부분
+                for(let i=0; i<data.length; i++) {
+                    listHtml += '<div class="Todo_Input item">';
+                    listHtml += '<input type="hidden" id="text" name="text" value="'+data.todoId+'"/>';
+                    listHtml += '<input type="hidden" id="text" name="text" value="'+data.created_at+'"/>';
+                    listHtml += '<input type="text" id="text" name="text" value="'+data.text+'"/>';
+                    listHtml += '<span class="btn edit" id="editBtn" name="editBtn">수정</sapn>';
+                    listHtml += '<span class="btn complete" id="saveBtn" name="saveBtn">완료</span>';
+                    listHtml += '<span class="btn remove" id="removeBtn" name="removeBtn">삭제</span>';
+                    listHtml += '<span class="btn cancel" id="cancelBtn" name="cancelBtn">취소</span>';
+                    listHtml += '</div>';
+                }
 
-                //JSON파싱
-                //JSON.parse(xmlhttp.responseText);
-                //UTF-8 변환
-                //encodeURIComponent('')
+                $(".Todo_Input list").html(listHtml);
 
-                document.getElementById('text').value = xmlhttp.responseText.text;
-
-            } else {
-                //alert('readTodoAjax fail');
+                $('#editBtn').show();
+                $('#saveBtn').hide();
+                $('#cancelBtn').hide();
+                $('#removeBtn').show();
             }
-        }
-        var userId = sessionStorage.getItem('userId');
 
-        xmlhttp.open("GET", "/api/todo", true);  
-        xmlhttp.send(userId);  
+        });
 
-	//}, createTodoView : function() {
+    }, createTodoAjax : function() {
 
-	}, createTodoAjax : function() {
-
-        //객체 생성부분
-        var xmlhttp;  
-        if (window.XMLHttpRequest) {  
-            xmlhttp = new XMLHttpRequest();
-        } 
-        else {  
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        const param = {
+            userId: $('#userId').val(),
+            text: $('#text').val()
         }
 
-        //Ajax구현부분
-        xmlhttp.onreadystatechange = function() {  
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                //통신 성공시 구현부분
+        $.ajax({
 
-                //JSON파싱
-                //JSON.parse(xmlhttp.responseText);
+            url: '${pageContext.request.contextPath}',
+            type: 'POST',
+            data: param,
+            //dataType: ,
+            success: function(data) {
                 
-                alert('저장이 완료되었습니다.');
+                if(data.msg == 'success') {
 
-            } else {
-                //alert('createTodo fail');
+                    alert('추가되었습니다.')
+                    TodoListController.readTodoAjax();
+                }
             }
-        }
-        var todoText = document.getElementById('text').value;
 
-        xmlhttp.open("POST", "/api/todo", true);  
-        xmlhttp.send(todoText);
+        });
 
-	//}, updateTodoView : function() {
-	
-	//}, updateTodoAjax : function() {
-	
-	}, deleteTodoAjax : function() {
+    }, deleteTodoAjax : function() {
 
-        //객체 생성부분
-        var xmlhttp;  
-        if (window.XMLHttpRequest) {  
-            xmlhttp = new XMLHttpRequest();
-        } 
-        else {  
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
+        $.ajax({
 
-        //Ajax구현부분
-        xmlhttp.onreadystatechange = function() {  
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                //통신 성공시 구현부분
-
-                //JSON파싱
-                //JSON.parse(xmlhttp.responseText);
+            url: '${pageContext.request.contextPath}/${todo_id}',
+            type: 'POST',
+            //dataType: ,
+            success: function(data) {
                 
-                alert('삭제가 완료되었습니다.');
+                if(data.msg == 'success') {
 
-            } else {
-                //alert('createTodo fail');
+                    alert('삭제되었습니다.')
+                    TodoListController.readTodoAjax();
+                }
             }
-        }
-        var todoId = document.getElementById('id').value;
 
-        xmlhttp.open("POST", "/api/todo", true);  
-        xmlhttp.send('id='+todoId);
-	}
+        });
+
+    }, updateTodoView : function() {
+
+        $('#editBtn').hide();
+        $('#saveBtn').show();
+        $('#cancelBtn').show();
+        $('#removeBtn').hide();
+
+
+    }, updateTodoAjax : function() {
+        
+        const param = {
+            userId: $('#userId').val(),
+            text: $('#text').val()
+        }
+
+        $.ajax({
+
+            url: '${pageContext.request.contextPath}',
+            type: 'POST',
+            data: param,
+            //dataType: ,
+            success: function(data) {
+                
+                if(data.msg == 'success') {
+
+                    alert('추가되었습니다.')
+                    TodoListController.readTodoAjax();
+                }
+            }
+
+        });
+
+    }, cancelUpdate: function() {
+
+        $('#editBtn').show();
+        $('#saveBtn').hide();
+        $('#cancelBtn').hide();
+        $('#removeBtn').show();
+    }
 }
+
